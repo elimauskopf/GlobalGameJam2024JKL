2176aa2586ef945c9c9dd7eb88ada73245b61f29
bumblebeangames <bumblebeangames@gmail.com>
2024-01-25 11:49:52 -0600

    Finished home menu (minus assets)

--- Assets/Scripts/HomeScreen.cs
+++ Assets/Scripts/HomeScreen.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class HomeScreen : MonoBehaviour
 {
+    GameObject _tutorial;
+    Animator _closeTutorialButton;
     // Start is called before the first frame update
     void Awake()
     {
-        
+        _tutorial = transform.Find("Tutorial").gameObject;
+        _closeTutorialButton = _tutorial.transform.GetChild(0).GetComponent<Animator>();
+        _tutorial.SetActive(false);
     }
 
     public void StartGame()
@@ -17,6 +21,18 @@ public class HomeScreen : MonoBehaviour
     }
     public void OpenTutorial()
     {
+        _tutorial.SetActive(true);
+    }
 
+    public void CloseTutorial()
+    {
+        _closeTutorialButton.SetTrigger("Press");
+        StartCoroutine(WaitToClose());
+    }
+
+    IEnumerator WaitToClose()
+    {
+        yield return new WaitForSeconds(0.3f);
+        _tutorial.SetActive(false);
     }
 }
